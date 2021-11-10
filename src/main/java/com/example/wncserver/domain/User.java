6f1ac2162b3d161f607208de40e3dab3718fb797
SeoSeunghun <proto_seo@naver.com>
2021-11-10 20:40:20 +0900

    [feat] user domain 생성

---
+++ src/main/java/com/example/wncserver/domain/User.java
@@ -0,0 +1,40 @@
+package com.example.wncserver.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@Getter
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Long id;
+
+	@Column(name = "email", nullable = false, unique = true)
+	private String email;
+
+	@Column(name = "password", nullable = false)
+	private String password;
+
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@Column(name = "image_url")
+	private String imageUrl;
+
+	@Column(name = "point")
+	private double point;
+
+	@Column(name = "ban_count")
+	private int banCount;
+}
