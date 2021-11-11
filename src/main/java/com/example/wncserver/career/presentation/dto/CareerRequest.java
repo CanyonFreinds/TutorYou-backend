5007c2871db0d42a7f59db8ac9826700d73b3548
SeoSeunghun <proto_seo@naver.com>
2021-11-11 12:35:41 +0900

    [feat][#3] Career 관련 DTO 구현

---
+++ src/main/java/com/example/wncserver/career/presentation/dto/CareerRequest.java
@@ -0,0 +1,12 @@
+package com.example.wncserver.career.presentation.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CareerRequest {
+	private String content;
+	private String careerType;
+}
+
