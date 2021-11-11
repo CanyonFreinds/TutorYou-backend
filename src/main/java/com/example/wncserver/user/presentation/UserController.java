3de58eef6ecdb9ee192d7cce4894a1fa65251c8e
SeoSeunghun <proto_seo@naver.com>
2021-11-11 17:45:20 +0900

    [feat][#7] user Image 관련 기능 Controller에 추가

--- src/main/java/com/example/wncserver/user/presentation/UserController.java
+++ src/main/java/com/example/wncserver/user/presentation/UserController.java
@@ -5,11 +5,14 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.wncserver.exception.custom.EmailDuplicateException;
 import com.example.wncserver.user.application.UserService;
@@ -58,5 +61,11 @@ public class UserController {
 		@RequestBody final UserNameUpdateRequest updateRequest) {
 		final boolean result = userService.changeUserName(updateRequest, userId);
 		return ResponseEntity.ok(result);
+	}
+
+	@PutMapping("/users/{userId}/image")
+	public ResponseEntity<String> changeUserProfileImage(@PathVariable final Long userId,
+		@RequestPart("image") final MultipartFile multipartFile) {
+		return ResponseEntity.ok(userService.changeUserImageUrl(multipartFile, userId));
 	}
 }
