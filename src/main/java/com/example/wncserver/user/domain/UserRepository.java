5117fd2481a2a9eddcc43465af782cb899c08f98
SeoSeunghun <proto_seo@naver.com>
2021-11-11 09:32:40 +0900

    [refactor][#1] 코드 구조 변경

--- src/main/java/com/example/wncserver/user/domain/UserRepository.java
+++ src/main/java/com/example/wncserver/user/domain/UserRepository.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.wncserver.domain.User;
+import com.example.wncserver.user.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByEmail(String email);
