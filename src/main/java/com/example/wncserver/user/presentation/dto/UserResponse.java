cca476fee077964e74de5182c53a20dc57aec63b
SeoSeunghun <proto_seo@naver.com>
2021-11-11 12:37:08 +0900

    [feat][#3] User 정보에 CareerResponse 추가

--- src/main/java/com/example/wncserver/user/presentation/dto/UserResponse.java
+++ src/main/java/com/example/wncserver/user/presentation/dto/UserResponse.java
@@ -1,5 +1,9 @@
 package com.example.wncserver.user.presentation.dto;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.example.wncserver.career.presentation.dto.CareerResponse;
 import com.example.wncserver.user.domain.User;
 
 import lombok.Builder;
@@ -11,14 +15,16 @@ public class UserResponse {
 	private final Long id;
 	private final String email;
 	private final String name;
-	private final String authority;
+	private final String role;
+	private final List<CareerResponse> careers;
 
 	public static UserResponse from(final User user) {
 		return UserResponse.builder()
 			.id(user.getId())
 			.name(user.getName())
 			.email(user.getEmail())
-			.authority(user.getRole().toString())
+			.role(user.getRole().toString())
+			.careers(user.getCareers().stream().map(CareerResponse::from).collect(Collectors.toList()))
 			.build();
 	}
 }
