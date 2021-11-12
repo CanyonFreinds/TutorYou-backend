9c15ca303fbfa1895236d5e23ffe84bb948af95b
SeoSeunghun <proto_seo@naver.com>
2021-11-12 19:39:02 +0900

    [feat][#10] groupId, studentId로 StudentGroup을 찾을 수 있도록 구현

--- src/main/java/com/example/wncserver/group/domain/StudentGroupRepository.java
+++ src/main/java/com/example/wncserver/group/domain/StudentGroupRepository.java
@@ -9,6 +9,7 @@ import com.example.wncserver.user.domain.User;
 
 public interface StudentGroupRepository extends JpaRepository<StudentGroup, Long> {
 	Optional<StudentGroup> findByGroupAndStudent(Group group, User student);
+	Optional<StudentGroup> findByGroup_IdAndStudent_Id(Long groupId, Long studentId);
 	List<StudentGroup> findAllByGroup(Group group);
 	List<StudentGroup> findAllByStudent(User student);
 }
