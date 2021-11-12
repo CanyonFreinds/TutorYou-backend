1bac3538984c68330a53b617575ee00fef8d05e5
SeoSeunghun <proto_seo@naver.com>
2021-11-12 16:30:07 +0900

    [feat][#8] Group, StudentGroup 관련 도메인 및 Repository 구현

---
+++ src/main/java/com/example/wncserver/group/domain/GroupRepository.java
@@ -0,0 +1,6 @@
+package com.example.wncserver.group.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+}
