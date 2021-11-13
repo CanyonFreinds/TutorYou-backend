7d277685ca86c64703fa00c69044ef1f41445cba
SeoSeunghun <proto_seo@naver.com>
2021-11-13 17:53:56 +0900

    [feat][#12] Notification 관련 도메인, Repository 생성

---
+++ src/main/java/com/example/wncserver/notification/domain/Notification.java
@@ -0,0 +1,47 @@
+package com.example.wncserver.notification.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.example.wncserver.user.domain.User;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "notification")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Notification {
+	@Id
+	@Column(name = "notification_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "message")
+	private String message;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User receiver;
+
+	public void setReceiver(User receiver) {
+		this.receiver = receiver;
+		receiver.getNotifications().add(this);
+	}
+
+	public static Notification createNotification(User receiver) {
+		Notification notification = new Notification();
+		notification.setReceiver(receiver);
+		notification.setMessage("과외 모집이 완료되었습니다.");
+		return notification;
+	}
+}
