f124043701d864534c70435632b341a86cfa759f
SeoSeunghun <proto_seo@naver.com>
2021-11-13 11:13:49 +0900

    [feat][#10] Teacher 관련 DTO

---
+++ src/main/java/com/example/wncserver/user/presentation/dto/teacher/TeacherResponse.java
@@ -0,0 +1,30 @@
+package com.example.wncserver.user.presentation.dto.teacher;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.example.wncserver.career.presentation.dto.CareerResponse;
+import com.example.wncserver.user.domain.User;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class TeacherResponse {
+	private final Long id;
+	private final String name;
+	private final double point;
+	private final int studentCount;
+	private final List<CareerResponse> careers;
+
+	public static TeacherResponse from(User teacher) {
+		return TeacherResponse.builder()
+			.id(teacher.getId())
+			.name(teacher.getName())
+			.point(teacher.getPoint())
+			.studentCount(teacher.getStudentCount())
+			.careers(teacher.getCareers().stream().map(CareerResponse::from).collect(Collectors.toList()))
+			.build();
+	}
+}
