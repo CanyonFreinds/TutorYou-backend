62fd7f4e8e7ffcbefba7095e5d7529d55af88c19
SeoSeunghun <proto_seo@naver.com>
2021-11-13 17:56:14 +0900

    [feat][#12] notification 관련 Controller dto 구현

---
+++ src/main/java/com/example/wncserver/notification/presentation/NotificationController.java
@@ -0,0 +1,31 @@
+package com.example.wncserver.notification.presentation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.wncserver.notification.application.NotificationService;
+import com.example.wncserver.support.handler.NotificationHandler;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class NotificationController {
+	private final NotificationService notificationService;
+
+	@DeleteMapping("/notifications/{notificationId}")
+	public ResponseEntity<Void> deleteNotification(@PathVariable final Long notificationId) {
+		notificationService.deleteNotification(notificationId);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PostMapping("/notifications/test/{userId}")
+	public ResponseEntity<Long> createNotification(@PathVariable final Long userId) {
+		return ResponseEntity.ok(notificationService.createNotification(userId));
+	}
+}
