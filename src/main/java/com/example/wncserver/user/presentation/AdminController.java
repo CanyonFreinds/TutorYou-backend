8306c780348609808f02bf31a835265225c5a3fd
SeoSeunghun <proto_seo@naver.com>
2021-11-13 11:15:39 +0900

    [feat][#10] admin controller 생성

---
+++ src/main/java/com/example/wncserver/user/presentation/AdminController.java
@@ -0,0 +1,13 @@
+package com.example.wncserver.user.presentation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class AdminController {
+
+}
