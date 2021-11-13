af2129ce815ee2ac6a3ac5521a0e0c6a514d6e55
SeoSeunghun <proto_seo@naver.com>
2021-11-13 17:55:13 +0900

    [feat][#12] socket 통신시 사용하는 message 구현

---
+++ src/main/java/com/example/wncserver/support/dto/WebSocketMessage.java
@@ -0,0 +1,22 @@
+package com.example.wncserver.support.dto;
+
+import java.util.List;
+
+import com.example.wncserver.notification.presentation.dto.NotificationResponse;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class WebSocketMessage {
+	private String type;
+	private Long userId;
+	private List<NotificationResponse> notifications;
+
+	public WebSocketMessage(String type, Long userId, List<NotificationResponse> notifications) {
+		this.type = type;
+		this.userId = userId;
+		this.notifications = notifications;
+	}
+}
