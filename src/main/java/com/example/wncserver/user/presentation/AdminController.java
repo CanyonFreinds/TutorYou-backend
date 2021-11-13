8f139bcf4689c148a94ef5be784c0302866a574c
SeoSeunghun <proto_seo@naver.com>
2021-11-13 14:12:56 +0900

    [feat][#14] Admin Controller 구현

--- src/main/java/com/example/wncserver/user/presentation/AdminController.java
+++ src/main/java/com/example/wncserver/user/presentation/AdminController.java
@@ -1,7 +1,16 @@
 package com.example.wncserver.user.presentation;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.wncserver.user.application.UserService;
+import com.example.wncserver.user.presentation.dto.admin.AdminResponse;
+import com.example.wncserver.user.presentation.dto.admin.AdminTeacherResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,5 +18,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class AdminController {
+	private final UserService userService;
 
+	@GetMapping("/admin/teacher/blacklist")
+	public ResponseEntity<AdminResponse> getBlackList(final Pageable pageable) {
+		return ResponseEntity.ok(userService.getTeacherListByIsBaned(true, pageable));
+	}
+
+	@GetMapping("/admin/teacher")
+	public ResponseEntity<AdminResponse> getTeacherList(final Pageable pageable) {
+		return ResponseEntity.ok(userService.getTeacherListByIsBaned(false, pageable));
+	}
+
+	@PutMapping("/admin/teacher/{teacherId}")
+	public ResponseEntity<AdminTeacherResponse> changeTeacherBanState(@PathVariable final Long teacherId) {
+		return ResponseEntity.ok(userService.manageBanTeacher(teacherId));
+	}
 }
