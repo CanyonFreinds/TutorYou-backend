32be2a8fdec666e8f11db39230e815a999665af1
SeoSeunghun <proto_seo@naver.com>
2021-11-14 16:36:47 +0900

    [feat][#18] chat domain 관련 수정

--- src/main/java/com/example/wncserver/chat/domain/ChatRoomUserRepository.java
+++ src/main/java/com/example/wncserver/chat/domain/ChatRoomUserRepository.java
@@ -1,6 +1,10 @@
 package com.example.wncserver.chat.domain;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomUserRepository extends JpaRepository<ChatRoomUser, Long> {
+	List<ChatRoomUser> findAllByChatter_Id(Long userId);
+	List<ChatRoomUser> findAllByChatRoom_Id(Long chatRoomId);
 }
